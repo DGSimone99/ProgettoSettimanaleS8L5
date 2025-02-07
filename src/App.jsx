e15ca0878d8a452c003d207775231fc344d02d06
DGSimone99 <simone.digiorgio99@gmail.com>
2025-02-07 17:42:31 +0100

    Commento pagine extra

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
 import User from "./components/User";
 import Settings from "./components/settings";
-import SelectPage from "./components/SelectPage";
+/* import SelectPage from "./components/SelectPage"; */
 
 function App() {
   const userImg = "../assets/User.jpg";
@@ -13,22 +13,24 @@ function App() {
   return (
     <div>
       <TopBar />
-      <SelectPage home="#home" user="#user" settings="#settings" />
+      {/* <SelectPage home="#home" user="#user" settings="#settings" /> */}
       {/* Tentativo andato male di provare a selezionare le pagine */}
       <div id="home">
         <Gallery movies="Alien" saga="Alien" />
         <Gallery movies="Pirates" saga="Pirates of the Caribbean + Extra" />
         <Gallery movies="Spider-Man" saga="Spider-Man" />
       </div>
-      <div id="user" className="d-none">
+      {/* <div id="user">
         <User img={userImg} name={userName} />
-      </div>
-      <div id="settings" className="d-none">
+      </div> */}
+      {/* <div id="settings">
         <Settings img={userImg} name={userName} />
-      </div>
+      </div> */}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+/* Ho tolto il className="d-none" a user e settings visto che il sistema di pagine che ho fatto era sbagliato */
